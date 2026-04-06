c9ecc89a1e50a6215cef327bfc1a25bfaa689405
Edrey Pomarez <pomarezedrey@gmail.com>
2026-04-05 19:54:54 -0600

    feat(Vista.FrmHome): Separate code into individual methods.
    Separate into individual methods the code that configurates
    the Grid's columns.
    This make the code cleaner and more maintanable.

--- GestorAutoMarket/GestorAutoMarket.Vista/FrmHome.cs
+++ GestorAutoMarket/GestorAutoMarket.Vista/FrmHome.cs
@@ -17,6 +17,8 @@ namespace GestorAutoMarket.Vista
             InitializeComponent();
         }
 
+        // Este método se ejecuta al cambiar de TabPage y recarga el GridView
+        // con la información correspondiente
         private void tCMenu_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -29,244 +31,8 @@ namespace GestorAutoMarket.Vista
             cargarGridView();
         }
 
-        // Este método lo abstraje para evitar repetir código, me permite obtener
-        // los datos para el gridview según el TabPage seleccionado
-        private void cargarGridView()
-        {
-            var selectedTab = tCMenu.SelectedTab;
-            if (selectedTab == null) return;
-            dgvInfoArreglos.DataSource = null;
-            dgvInfoArreglos.AutoGenerateColumns = false;
-            dgvInfoArreglos.Columns.Clear();
-
-            switch (selectedTab.Text)
-            {
-                case "Categorías de Vehículo":
-                    dgvInfoArreglos.DataSource = CategoriaVehiculoLN.getCategoriasVehiculo();
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "idCategoria",
-                        HeaderText = "ID"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "nombreCategoria",
-                        HeaderText = "Categoría"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "descripcion",
-                        HeaderText = "Descripción"
-                    });
-
-                    break;
-                case "Vehiculos":
-                    dgvInfoArreglos.DataSource = VehiculoLN.getVehiculos();
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "idVehiculo",
-                        HeaderText = "ID"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "marca",
-                        HeaderText = "Marca"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "modelo",
-                        HeaderText = "Modelo"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "anio",
-                        HeaderText = "Año"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "precio",
-                        HeaderText = "Precio"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "categoriaId",
-                        HeaderText = "ID Categoría"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "nombreCategoria",
-                        HeaderText = "Categoría"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "descripcionCategoria",
-                        HeaderText = "Descripción de Categoría"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "estado",
-                        HeaderText = "Estado"
-                    });
-
-                    break;
-                case "Clientes":
-                    dgvInfoArreglos.DataSource = ClienteLN.getClientes();
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fechaRegistro",
-                        HeaderText = "Fecha de Registro"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "id",
-                        HeaderText = "ID"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "identificacion",
-                        HeaderText = "Identificación"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "nombreCompleto",
-                        HeaderText = "Nombre"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fehaNacimiento",
-                        HeaderText = "Fecha de Nacimiento"
-                    });
-
-                    break;
-                case "Sucursales":
-                    dgvInfoArreglos.DataSource = SucursalLN.getSucursales();
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "idSucursal",
-                        HeaderText = "ID"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "nombreSucursal",
-                        HeaderText = "Nombre"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "direccion",
-                        HeaderText = "Dirección"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "telefono",
-                        HeaderText = "Teléfono"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "encargado",
-                        HeaderText = "Encargado"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "esActiva",
-                        HeaderText = "Activa"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "encargadoID",
-                        HeaderText = "ID de Encargado"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "encargadoNombre",
-                        HeaderText = "Nombre de Encargado"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fechaNacimientoEncargado",
-                        HeaderText = "Fecha de nacimiento de Encargado"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fechaIngresoENcargado",
-                        HeaderText = "Fecha de Ingreso de Encargado"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "telefonoEncargado",
-                        HeaderText = "Teléfono de Encargado"
-                    });
-
-
-                    break;
-                case "Vendedores":
-                    dgvInfoArreglos.DataSource = VendedorLN.getVendedores();
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fechaIngreso",
-                        HeaderText = "Fecha de Ingreso"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "telefono",
-                        HeaderText = "Teléfono"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "id",
-                        HeaderText = "ID"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "identificacion",
-                        HeaderText = "Identificación"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "nombreCompleto",
-                        HeaderText = "Nombre Completo"
-                    });
-
-                    dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
-                    {
-                        DataPropertyName = "fechaNacimiento",
-                        HeaderText = "Fecha de Nacimiento"
-                    });
-
-                    break;
-
-            }
-        }
-
+        // Este método se ejecuta al hacer clic en el botón "Añadir Elemento" 
+        // y abre el formulario correspondiente según el TabPage seleccionado
         private void btnAniadirElemento_Click(object sender, EventArgs e)
         {
             var selectedTab = tCMenu.SelectedTab;
@@ -306,5 +72,161 @@ namespace GestorAutoMarket.Vista
                     break;
             }
         }
+
+        // Este método carga el DataSource en el GridView según el TabPage seleccionado
+        private void cargarGridView()
+        {
+            var selectedTab = tCMenu.SelectedTab;
+            if (selectedTab == null) return;
+            dgvInfoArreglos.DataSource = null;
+            dgvInfoArreglos.AutoGenerateColumns = false;
+            dgvInfoArreglos.Columns.Clear();
+
+            switch (selectedTab.Text)
+            {
+                case "Categorías de Vehículo":
+                    dgvInfoArreglos.DataSource = CategoriaVehiculoLN.getCategoriasVehiculo();
+                    ConfigurarColumnasCategorias();
+                    break;
+                case "Vehiculos":
+                    dgvInfoArreglos.DataSource = VehiculoLN.getVehiculos();
+                    ConfigurarColumnasVehiculos();
+                    break;
+                case "Clientes":
+                    dgvInfoArreglos.DataSource = ClienteLN.getClientes();
+                    ConfigurarColumnasClientes();
+
+                    break;
+                case "Sucursales":
+                    dgvInfoArreglos.DataSource = SucursalLN.getSucursales();
+                    ConfigurarColumnasSucursales();
+                    break;
+                case "Vendedores":
+                    dgvInfoArreglos.DataSource = VendedorLN.getVendedores();
+                    ConfigurarColumnasVendedores();
+
+                    break;
+
+            }
+        }
+        
+        // Estos métodos permiten configurar
+        // las columnas para el gridview según el TabPage seleccionado
+        private  void ConfigurarColumnasCategorias()
+        {
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "idCategoria",
+                HeaderText = "ID"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "nombreCategoria",
+                HeaderText = "Categoría"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "descripcion",
+                HeaderText = "Descripción"
+            });
+        }
+        private void ConfigurarColumnasClientes()
+        {
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "id",
+                HeaderText = "ID"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "identificacion",
+                HeaderText = "Identificación"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "nombreCompleto",
+                HeaderText = "Nombre Completo"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "fehaNacimiento",
+                HeaderText = "Fecha de Nacimiento"
+            });
+        }
+        private void ConfigurarColumnasSucursales()
+        {
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "idSucursal",
+                HeaderText = "ID"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "nombreSucursal",
+                HeaderText = "Nombre"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "direccion",
+                HeaderText = "Dirección"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "telefono",
+                HeaderText = "Teléfono"
+            });
+        }
+        private void ConfigurarColumnasVehiculos()
+        {
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "idVehiculo",
+                HeaderText = "ID"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "marca",
+                HeaderText = "Marca"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "modelo",
+                HeaderText = "Modelo"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "anio",
+                HeaderText = "Año"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "precio",
+                HeaderText = "Precio"
+            });
+        }
+        private void ConfigurarColumnasVendedores()
+        {
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "id",
+                HeaderText = "ID"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "identificacion",
+                HeaderText = "Identificación"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "nombreCompleto",
+                HeaderText = "Nombre Completo"
+            });
+            dgvInfoArreglos.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "fechaNacimiento",
+                HeaderText = "Fecha de Nacimiento"
+            });
+        }
+
     }
 }
